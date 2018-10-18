2b1798c3858a4e6343f9990a159672099f528682
takeokunn <bararararatty@gmail.com>
2018-10-18 13:51:07 +0900

    Update

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,9 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
+listen '/tmp/unicorn.sock'
+pid '/tmp/unicorn.pid'
+
 before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
